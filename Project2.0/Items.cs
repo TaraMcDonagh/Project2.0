6c3deeed84a0578d6b7f6989788b347e5df835e8
Tara McDonagh <Taramcdonagh@hotmail.com>
2020-04-16 15:04:58 +0100

    Add project files.

---
+++ Project2.0/Items.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2._0
+{
+    public enum ProductType
+    {
+        Face, Eyes, Lips
+    }
+    public class Items : IComparable<Items>
+    {
+        //These are my properties
+        private string _description;
+        public string ProductName { get; set; }
+
+        public string Brand { get; set; }
+
+        public decimal Cost { get; set; }
+        public ProductType TypeOfProduct { get; set; }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        //these are my constructers
+        public Items()
+        {
+
+        }
+        public Items(string name, string description, string brand, ProductType type, decimal cost)
+        {
+            ProductName = name;
+            Description = description;
+            Brand = brand;
+            TypeOfProduct = type;
+            Cost = cost;
+        }
+        //Methods
+        public override string ToString()
+        {
+            return $"{ProductName} - {Cost}";
+        }
+        //Implement IComparable to sort by date, utilised by both listboxes. 
+        public int CompareTo(Items other)
+        {
+            if (this.TypeOfProduct == other.TypeOfProduct)
+            {
+                return this.ProductName.CompareTo(other.ProductName);
+            }
+            return other.TypeOfProduct.CompareTo(this.TypeOfProduct);
+        }
+    }
+}
---
+++ Project2.0/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project2._0
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Items> allProducts = new List<Items>();
+        List<Items> selectedProducts = new List<Items>();
+        List<Items> filteredProducts = new List<Items>();
+        decimal total = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //create some activity objects
+            Items l1 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items l2 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items l3 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items l4 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items e1 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items e2 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items e3 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items e4 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+            Items f1 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items f2 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items f3 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+
+            Items f4 = new Items()
+            {
+                ProductName = "",
+                Description = "",
+                Brand = "Tarte",
+                TypeOfProduct = ProductType.Lips,
+                Cost = 35m
+            };
+            //adding the activities to the list created above
+            allProducts.Add(l1);
+            allProducts.Add(l2);
+            allProducts.Add(l3);
+            allProducts.Add(l4);
+            allProducts.Add(e1);
+            allProducts.Add(e2);
+            allProducts.Add(e3);
+            allProducts.Add(e4);
+            allProducts.Add(f1);
+            allProducts.Add(f2);
+            allProducts.Add(f3);
+            allProducts.Add(f4);
+            allProducts.Sort();
+            allProducts.Reverse();
+
+            //Displaying them in the listbox
+            lbxProducts.ItemsSource = allProducts;
+        }
+
+        private void AddToCartBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
